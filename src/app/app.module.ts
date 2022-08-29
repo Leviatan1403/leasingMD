e0db1333e7e9ec26d28581e63f63e46a4c0809fe
Manuel Guillen <manuelguillen1403@hotmail.com>
2022-08-28 21:00:24 -0500

    Finalizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -36,6 +37,7 @@ import { FooterComponent } from './components/footer/footer.component';
     MatSelectModule,
     MatButtonModule,
     MatFormFieldModule,
+    MatMenuModule,
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule
